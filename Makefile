e4372f8a2c1856ae6d346e5a73a650fb9bf99d0e
XiaochenCui <jcnlcxc.new@gmail.com>
2024-11-05 09:45:07 -0800

    update

---
+++ Makefile
@@ -0,0 +1,4 @@
+run_benchmark:
+	./scripts/run_benchmark.py
+
+all: run_benchmark
---
+++ scripts/run_benchmark.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+# io_uring vs epoll
+def bench_a():
+    print("bench_a")
+
+
+if __name__ == "__main__":
+    bench_a()
